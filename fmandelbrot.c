c96a1833ad1da1bdc8ed6c8abf785724879fdcf3
Fin <spamsuckersunited@gmail.com>
2019-03-05 18:01:49 -0700

    Artsier signature with more pretty comments

--- fmandelbrot.c
+++ fmandelbrot.c
@@ -1,7 +1,6 @@
-int main(){double x,y,u,v,w,z;int 
-i,j,k;for(j=0;j<33;j++){y=1-j*(2./
-33);for(i=0;i<80;i++){u=0;v=0;w=u*
-u;z=v*v;x=i*.0375-2;for(k=1;k<2e3&
-(w+z<4);k++)v=2*u*v+y,u=w-z+x,w=u*
-u,z=v*v;putchar(k>=27?32:97+k/3);}
-putchar(10);}}/*  Fin Blackett  */
+/*   #######*/double x,y,u,v,w,z;int i,j,k;int main(
+/*   #      */){for(j=0;j<33;j++){y=1-j*2./33;for(i=
+/*  ####    */0;i<80;i++){u=v=w=z=0;x=i*.0375-2;for(
+/*  #       */k=1;k<2e3&&(w+z<4);k++){v=2*u*v+y;u=w-
+/* #        */z+x;w=u*u;z=v*v;}putchar(k>26?32:97+k/
+/*Finian Blackett Infosec*/3);}putchar(10);}}/*GCC*/
