e2d168ee10a2dccb9c93ddb721f828806bcc7099
Fin <spamsuckersunited@gmail.com>
2019-03-07 14:49:27 -0700

    Rename and add business card version

---
+++ business_card_mandel.c
@@ -0,0 +1,12 @@
+      main(k){for(float
+     x,y,u,v,z;++y<40;
+     puts(""))for(x=-
+    2;x+=.03
+   ,x<1;putchar(k+
+   32))for(u=v=0,
+   k=27;z=v*v,--
+  k&&u*u+z
+  <4;u=u*u 
+ -z+x)v//  finian.blackett@gmail.com
+ =2*u*v+y
+/20-1;}//  Python Dev | Infosecurity
