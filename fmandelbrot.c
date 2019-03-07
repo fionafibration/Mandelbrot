45cf58eb2082460e852fc120e725ac2961d06cbd
Fin <spamsuckersunited@gmail.com>
2019-03-07 14:46:11 -0700

    Change spacing

--- fmandelbrot.c
+++ fmandelbrot.c
@@ -1,6 +1,6 @@
 /*  #######*/main(k){for(float x,y,u,v,z;++y<40;puts
 /*  #      */(""))for(x=-2;x+=.03,x<1;putchar(k+32))
 /* ####    */for(u=v=0,k=27;z=v*v,--k&&u*u+z<4;u=u*u
-/* #       */-z+x)v=2*u*v+y/20-1;}//Python Developer
-/*#        Finian Blackett | Information Security */
+/* #       */-z+x)v=2*u*v+y/20-1;}/* C Mandelbrot */
+/*#  Finian Blackett | Infosec | Python Developer */
 /*--------=[ finian.blackett@gmail.com ]=---------*/
